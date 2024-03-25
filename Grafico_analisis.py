6122f1571971e1c9252e2802e479369af13c552b
Ricardo A. Hernández Dania <157486630+RicardoAHD@users.noreply.github.com>
2024-03-25 16:37:40 +0100

    Add files via upload

---
+++ Grafico_analisis.py
@@ -0,0 +1,134 @@
+"""Script to analyze data from a CSV file."""
+
+import pandas as pd
+import matplotlib.pyplot as plt
+import numpy as np
+from sklearn.linear_model import LinearRegression
+file_path = 'test_tinsa_data.csv'  
+data = pd.read_csv(file_path, delimiter=';')
+
+
+# Dividir los inmuebles según su superficie
+grandes = data[data['SUPERFICIE'] > 165]
+medianos = data[(data['SUPERFICIE'] >= 53) & (data['SUPERFICIE'] <= 165)]
+pequenos = data[data['SUPERFICIE'] < 53]
+# Dividir los inmuebles según si tienen ascensor
+con_ascensor = data[data['ASCENSOR'] == 'si']
+sin_ascensor = data[data['ASCENSOR'] == 'no']
+# Dividir los inmuebles según su antigüedad
+nuevos = data[data['ANTIGUEDAD'] <= 15]
+medios = data[(data['ANTIGUEDAD'] > 15) & (data['ANTIGUEDAD'] <= 46)]
+viejos = data[data['ANTIGUEDAD'] > 46]
+# Dividir los inmuebles según si son exteriores o interiores
+exteriores = data[data['EXTERIOR_INTERIOR'] == 'EXTERIOR']
+interiores = data[data['EXTERIOR_INTERIOR'] == 'INTERIOR']
+
+#_______________________________________________________________________________________________
+# Relación Antigüedad/Precios de Inmuebles
+def plot_trend_line(x, y, label):
+    model = LinearRegression().fit(x.values.reshape(-1, 1), y)
+    plt.plot(x, model.predict(x.values.reshape(-1, 1)), color='black', linestyle='--', label=label)
+# Graficar los puntos y las líneas de tendencia para cada grupo
+precio_medio = data['VALOR'].mean()
+antiguedad_media = data['ANTIGUEDAD'].mean()
+# Graficar los puntos y las líneas de tendencia para cada grupo
+plt.figure(figsize=(10, 6))
+plt.scatter(nuevos['ANTIGUEDAD'], nuevos['VALOR'], color='blue', label='Nuevos(<15 años)')
+plot_trend_line(nuevos['ANTIGUEDAD'], nuevos['VALOR'], label='Tendencia Nuevos')
+plt.scatter(medios['ANTIGUEDAD'], medios['VALOR'], color='green', label='Medios(15<x<46 años)')
+plot_trend_line(medios['ANTIGUEDAD'], medios['VALOR'], label='Tendencia Medios')
+plt.scatter(viejos['ANTIGUEDAD'], viejos['VALOR'], color='red', label='Viejos(>46 años)')
+plot_trend_line(viejos['ANTIGUEDAD'], viejos['VALOR'], label='Tendencia Viejos')
+plt.xlabel('Antigüedad (años)')
+plt.ylabel('Valor (€)')
+plt.title('Relación Antigüedad/ Valor de Inmuebles')
+plt.legend()
+plt.grid(True)
+plt.show()
+#________________________________________________________________________________________________________
+
+
+# Relación Superficie/Precio de Inmuebles
+def plot_trend_line_and_mean(x, y, color, label):
+    # Ajustar la línea de tendencia
+    model = LinearRegression().fit(x.values.reshape(-1, 1), y)
+    x_range = np.linspace(min(x), max(x), 100).reshape(-1, 1)
+    y_pred = model.predict(x_range)
+    plt.plot(x_range, y_pred, 'black', linestyle='--', label=f'Línea de tendencia ({label})')
+    mean_x = np.mean(x)
+    mean_y = np.mean(y)
+    plt.scatter(mean_x, mean_y, color='yellow', marker='x', s=100, label=f'Media ({label})')
+plt.figure(figsize=(10, 6))
+plt.scatter(grandes['SUPERFICIE'], grandes['VALOR'], color='blue', label='Grandes (>165 m²)')
+plot_trend_line_and_mean(grandes['SUPERFICIE'], grandes['VALOR'], color='blue', label='Grandes (>165 m²)')
+plt.scatter(medianos['SUPERFICIE'], medianos['VALOR'], color='green', label='Medianos (53-165 m²)')
+plot_trend_line_and_mean(medianos['SUPERFICIE'], medianos['VALOR'], color='green', label='Medianos (53-165 m²)')
+plt.scatter(pequenos['SUPERFICIE'], pequenos['VALOR'], color='red', label='Pequeños (<53 m²)')
+plot_trend_line_and_mean(pequenos['SUPERFICIE'], pequenos['VALOR'], color='red', label='Pequeños (<53 m²)')
+plt.xlabel('Superficie (m²)')
+plt.ylabel('Valor (€)')
+plt.title('Relación Superficie/ Valor de Inmuebles')
+# Leyenda y cuadrícula
+plt.legend()
+plt.grid(True)
+plt.show()
+
+#_________________________________________________________________________________________________________________________________
+exterior_con_ascensor = data[(data['ASCENSOR'] == 'si') & (data['EXTERIOR_INTERIOR'] == 'EXTERIOR')]
+exterior_sin_ascensor = data[(data['ASCENSOR'] == 'no') & (data['EXTERIOR_INTERIOR'] == 'EXTERIOR')]
+interior_con_ascensor = data[(data['ASCENSOR'] == 'si') & (data['EXTERIOR_INTERIOR'] == 'INTERIOR')]
+interior_sin_ascensor = data[(data['ASCENSOR'] == 'no') & (data['EXTERIOR_INTERIOR'] == 'INTERIOR')]
+# Calcular el precio promedio para cada grupo
+precio_promedio_ext_con_ascensor = exterior_con_ascensor.groupby('DORMITORIOS')['VALOR'].mean()
+precio_promedio_ext_sin_ascensor = exterior_sin_ascensor.groupby('DORMITORIOS')['VALOR'].mean()
+precio_promedio_int_con_ascensor = interior_con_ascensor.groupby('DORMITORIOS')['VALOR'].mean()
+precio_promedio_int_sin_ascensor = interior_sin_ascensor.groupby('DORMITORIOS')['VALOR'].mean()
+# Crear un DataFrame con los precios PROMEDIO para cada grupo
+df_precios = pd.DataFrame({
+    'Exterior con Ascensor': precio_promedio_ext_con_ascensor,
+    'Exterior sin Ascensor': precio_promedio_ext_sin_ascensor,
+    'Interior con Ascensor': precio_promedio_int_con_ascensor,
+    'Interior sin Ascensor': precio_promedio_int_sin_ascensor})
+df_precios.plot(kind='bar', figsize=(10, 6))
+plt.xlabel('Número de Habitaciones')
+plt.ylabel('Valor Promedio (€)')
+plt.title('Relación Habitaciones/Valor medio de los Inmuebles()')
+plt.legend(title='Tipo de Piso')
+plt.xticks(rotation=0)
+plt.grid(axis='y')
+plt.show()
+
+#______________________________________________________________________________________________________________________________
+#Crear la cuarta tabla: Relación Código Postal/Precio de Inmuebles
+tipologias = data['TIPOLOGIA'].unique()
+
+# Datos
+codigos_postales = exteriores['CODPOS'].unique()
+precios_exteriores = {cp: {tipologia: exteriores[(exteriores['CODPOS'] == cp) & (exteriores['TIPOLOGIA'] == tipologia)]['VALOR'].mean() for tipologia in tipologias} for cp in codigos_postales}
+precios_interiores = {cp: {tipologia: interiores[(interiores['CODPOS'] == cp) & (interiores['TIPOLOGIA'] == tipologia)]['VALOR'].mean() for tipologia in tipologias} for cp in codigos_postales}
+
+# Crear gráfico de barras apiladas
+plt.figure(figsize=(12, 8))
+
+# Definir colores para cada tipo de vivienda
+colors = ['blue', 'green', 'red', 'orange', 'purple', 'brown', 'pink', 'gray']
+
+bottom = None
+for idx, tipologia in enumerate(tipologias):
+    precios_tipologia_exteriores = [precios_exteriores[cp][tipologia] for cp in codigos_postales]
+    precios_tipologia_interiores = [precios_interiores[cp][tipologia] for cp in codigos_postales]
+    
+    plt.bar(codigos_postales, precios_tipologia_interiores, bottom=bottom, color=colors[idx], label=f'{tipologia} - Interior')
+    plt.bar(codigos_postales, precios_tipologia_exteriores, bottom=bottom, color=colors[idx], alpha=0.5, label=f'{tipologia} - Exterior')
+    
+    if bottom is None:
+        bottom = [0] * len(precios_tipologia_interiores)
+    bottom = [bottom[i] + precios_tipologia_interiores[i] for i in range(len(precios_tipologia_interiores))]
+
+plt.xlabel('Código Postal')
+plt.ylabel('Valor Promedio (€)')
+plt.title('Relación Código Postal/Valor de Inmuebles por Tipología')
+plt.xticks(rotation=90)
+plt.legend()
+plt.grid(True)
+plt.show()
---
+++ análisis_PDF.py
@@ -0,0 +1,208 @@
+"""
+Script to analyze data from a CSV file.
+"""
+
+import csv
+import pandas as pd
+import plotly.graph_objs as go
+from tabulate import tabulate
+from datetime import datetime, timedelta
+from plotly.subplots import make_subplots
+import plotly.io as pio
+from reportlab.lib.pagesizes import letter
+from reportlab.platypus import SimpleDocTemplate, Table, TableStyle
+from reportlab.lib import colors
+
+
+
+ARCHIVO_CSV = 'test_tinsa_data.csv'                                     ##IMPORTANTE CAMBIAR LA RUTA RELATIVA PATH PARA CADA ARCHIVO QUE QUERAMOS ANALIZAR##
+
+
+# Contadores
+contador_unifamiliares = 0
+contador_plurifamiliares = 0
+contador_tasaciones_vigentes = 0
+
+contador_tasaciones_caducadas = 0
+contador_tasaciones_primer_cuatrimestre = 0
+contador_tasaciones_segundo_cuatrimestre = 0
+contador_tasaciones_tercer_cuatrimestre = 0
+contador_ascensor_si = 0
+contador_ascensor_no = 0
+contador_exterior = 0
+contador_interior = 0
+
+
+# Variables para estadísticas
+valores = []
+superficies = []
+dormitorios = []
+banos = []
+antiguedades = []
+fecha_consulta = datetime.now()
+pisos_por_codigo_postal = {}
+
+# Abrir el archivo CSV en modo lectura
+with open(ARCHIVO_CSV, 'r', encoding='utf-8') as file:
+    lector_csv = csv.DictReader(file, delimiter=';')
+    
+# Usos cada fila del CSV
+    for fila in lector_csv:
+        if fila['TIPOLOGIA'] == 'UNIFAMILIAR':
+            contador_unifamiliares += 1
+        elif fila['TIPOLOGIA'] == 'PLURIFAMILIAR':
+            contador_plurifamiliares += 1
+        fecha_tasacion = datetime.strptime(fila['FECHA'], '%Y-%m')
+        diferencia_tiempo = fecha_consulta - fecha_tasacion
+        if diferencia_tiempo <= timedelta(days=180):
+            contador_tasaciones_vigentes += 1
+        else:
+            contador_tasaciones_caducadas += 1
+        mes = fecha_tasacion.month
+        if mes >= 1 and mes <= 4:
+            contador_tasaciones_primer_cuatrimestre += 1
+        elif mes >= 5 and mes <= 8:
+            contador_tasaciones_segundo_cuatrimestre += 1
+        elif mes >= 9 and mes <= 12:
+            contador_tasaciones_tercer_cuatrimestre += 1
+        if fila['ASCENSOR'] == 'si':
+            contador_ascensor_si += 1
+        else:
+            contador_ascensor_no += 1
+        if fila['EXTERIOR_INTERIOR'] == 'EXTERIOR':
+            contador_exterior += 1
+        else:
+            contador_interior += 1
+
+      #CODIGOS POSTAL                                                                      +++++++++++++++++++++++++++++++++++++++++++++++++IDEA
+       # codigo_postal = fila['CODPOS']
+    # Verificar si el código postal ya está en el diccionario
+    #if codigo_postal in pisos_por_codigo_postal:
+        # Si ya existe, incrementar el contador
+       # pisos_por_codigo_postal[codigo_postal] += 1
+    #else:
+        # Si es la primera vez que se encuentra, inicializar el contador en 1
+        #pisos_por_codigo_postal[codigo_postal] = 1                                         +++++++++++++++++++++++++++++++++++++++++++++++++IDEA
+
+
+#Valores para estadísticas
+        valores.append(float(fila['VALOR']))
+        superficies.append(float(fila['SUPERFICIE']))
+        dormitorios.append(float(fila['DORMITORIOS']))
+        banos.append(float(fila['BANNOS']))
+        antiguedades.append(float(fila['ANTIGUEDAD']))    
+
+# Calcular el número total de viviendas
+numero_total_viviendas = contador_unifamiliares + contador_plurifamiliares
+
+#Nro total de vivienda CDMUN                                                               +++++++++++++++++++++++++++INFORMACION DEL INE
+total_viviendas_familiares = 254658
+total_viviendas_construidas = 254736
+total_viviendas_principales = 211358
+
+#% Vivienda analizadas respecto al total                                                    +++++++++++++++++++++++++ Si el total % es insignifcante(<60%) uso interno
+porcentaje_viviendas_familiares = (numero_total_viviendas / total_viviendas_familiares) * 100
+porcentaje_viviendas_construidas = (numero_total_viviendas / total_viviendas_construidas) * 100
+porcentaje_viviendas_principales = (numero_total_viviendas / total_viviendas_principales) * 100
+porcentaje_viviendas_familiares_formateado = "{:.2f}%".format(porcentaje_viviendas_familiares)
+porcentaje_viviendas_construidas_formateado = "{:.2f}%".format(porcentaje_viviendas_construidas)
+porcentaje_viviendas_principales_formateado = "{:.2f}%".format(porcentaje_viviendas_principales)
+
+#Tipologia de vivienda 
+porcentaje_unifamiliares = (contador_unifamiliares / numero_total_viviendas) * 100
+porcentaje_plurifamiliares = (contador_plurifamiliares / numero_total_viviendas) * 100
+
+#tasaciones (vigencia 180días)
+porcentaje_tasaciones_vigentes = (contador_tasaciones_vigentes / numero_total_viviendas) * 100
+porcentaje_tasaciones_caducadas = (contador_tasaciones_caducadas / numero_total_viviendas) * 100
+
+#Tasaciones por cuatrimestres 
+total_tasaciones = numero_total_viviendas
+porcentaje_tasaciones1 = (contador_tasaciones_primer_cuatrimestre/ total_tasaciones) * 100
+porcentaje_tasaciones2= (contador_tasaciones_segundo_cuatrimestre / total_tasaciones) * 100
+porcentaje_tasaciones3 =(contador_tasaciones_tercer_cuatrimestre / total_tasaciones) * 100
+
+#Valores
+valor_medio = sum(valores) / len(valores)
+valor_maximo = max(valores)
+valor_minimo = min(valores)
+superficie_minimo = min(superficies)
+superficie_maximo = max(superficies)
+superficie_media = sum(superficies) / len(superficies)
+dormitorios_minimo = min(dormitorios)
+dormitorios_maximo = max(dormitorios)
+dormitorios_media = sum(dormitorios) / len(dormitorios)
+banos_media = sum(banos) / len(banos)
+banos_maximo= max(banos)
+banos_minimo= min(banos)
+antiguedad_media = sum(antiguedades) / len(antiguedades)
+antiguedad_minima = min (antiguedades) 
+antiguedad_maxima = max(antiguedades)
+total_pisos = numero_total_viviendas
+
+#Calcular los porcentajes de ascensor
+porcentaje_ascensor_si = (contador_ascensor_si / total_pisos) * 100
+porcentaje_ascensor_no = (contador_ascensor_no / total_pisos) * 100
+
+#Calcular los porcentajes de exterior/interior
+porcentaje_exterior = (contador_exterior / total_pisos) * 100
+porcentaje_interior = (contador_interior / total_pisos) * 100
+
+
+#Representación gráfica
+
+
+def generate_pdf(results, filename):
+    doc = SimpleDocTemplate(filename, pagesize=letter)
+    elements = []
+    data = [['Descripción', 'Valor']]
+    data.extend(results)
+    t = Table(data)
+    t.setStyle(TableStyle([('BACKGROUND', (0, 0), (-1, 0), colors.lavender),
+                           ('TEXTCOLOR', (0, 0), (-1, 0), colors.black),
+                           ('ALIGN', (0, 0), (-1, -1), 'LEFT'),
+                           ('FONTNAME', (0, 0), (-1, 0), 'Helvetica-Bold'),
+                           ('BOTTOMPADDING', (0, 0), (-1, 0), 12),
+                           ('BACKGROUND', (0, 1), (-1, -1), colors.paleturquoise),
+                           ('GRID', (0, 0), (-1, -1), 1, colors.black)]))
+    elements.append(t)
+    doc.build(elements)
+
+if __name__ == "__main__":
+    results = [
+        ("Nro total de viviendas en el estudio", numero_total_viviendas),
+        ("Viviendas en estudio respecto al total de viviendas familiares", 
+         f"{porcentaje_viviendas_familiares_formateado} de {total_viviendas_familiares}"),
+        ("Viviendas en estudio respecto al total de viviendas construidas", 
+         f"{porcentaje_viviendas_construidas_formateado} de {total_viviendas_construidas}"),
+        ("Viviendas en estudio respecto al total de viviendas principales", 
+         f"{porcentaje_viviendas_principales_formateado} de {total_viviendas_principales}"),
+        ("Viviendas unifamiliares analizadas", "{:.2f}%".format(porcentaje_unifamiliares)),
+        ("Viviendas plurifamiliares analizadas", "{:.2f}%".format(porcentaje_plurifamiliares)),
+        ("Porcentaje de tasaciones vigentes", "{:.2f}%".format(porcentaje_tasaciones_vigentes)),
+        ("Porcentaje de tasaciones caducadas", "{:.2f}%".format(porcentaje_tasaciones_caducadas)),
+        ("% tasaciones 1er cuatri.", "{:.2f}%".format(porcentaje_tasaciones1)),
+        ("% tasaciones 2er cuatri.", "{:.2f}%".format(porcentaje_tasaciones2)),
+        ("% tasaciones 3er cuatri.", "{:.2f}%".format(porcentaje_tasaciones3)),
+        ("Valor Medio", f"{valor_medio:.2f} €"),
+        ("Valor Máximo", f"{valor_maximo:.2f} €"),
+        ("Valor Mínimo", f"{valor_minimo:.2f} €"),
+        ("Superficie Mínima", f"{superficie_minimo:.2f} metros^2"),
+        ("Superficie Máxima", f"{superficie_maximo:.2f} metros^2"),
+        ("Superficie Media", f"{superficie_media:.2f} metros^2"),
+        ("Dormitorios Mínimo", f"{dormitorios_minimo:.2f} unidades"),
+        ("Dormitorios Máximo", f"{dormitorios_maximo:.2f} unidades"),
+        ("Dormitorios Media", f"{dormitorios_media:.2f} unidades"),
+        ("Baños Máximo", f"{banos_maximo:.2f} unidades"),
+        ("Baños Mínimo", f"{banos_minimo:.2f} unidades"),
+        ("Baños Media", f"{banos_media:.2f} unidades"),
+        ("Antigüedad Máxima", f"{antiguedad_maxima:.2f} años"),
+        ("Antigüedad Mínima", f"{antiguedad_minima:.2f} años"),
+        ("Antigüedad Media", f"{antiguedad_media:.2f} años"),
+        ("Porcentaje de pisos con ascensor", f"{porcentaje_ascensor_si:.2f}%"),
+        ("Porcentaje de pisos sin ascensor", f"{porcentaje_ascensor_no:.2f}%"),
+        ("Porcentaje de pisos exteriores", f"{porcentaje_exterior:.2f}%"),
+        ("Porcentaje de pisos interiores", f"{porcentaje_interior:.2f}%")
+    ]
+    filename = "resultados.pdf"
+    generate_pdf(results, filename)
